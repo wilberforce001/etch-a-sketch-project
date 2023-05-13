d4806ffe3aef6309001f39fbe250545d198623b4
wilberforce001 <kenmisik@gmail.com>
2023-05-13 17:21:00 +0300

    Set functionality that tells whether we can draw or not

--- index.js
+++ index.js
@@ -50,6 +50,13 @@ function resetBoard() {
     squares.forEach((div) => div.style.backgroundColor = ("white"));
 }
 
-document.querySelector("body").addEventListener("click", () => {
-    click = !click;
+document.querySelector("body").addEventListener("click", (e) => {
+    if (e.target.tagName != "BUTTON") {
+        click = !click;
+    if(click) {
+        document.querySelector('.mode').textContent = "Mode: Coloring"
+    } else {
+        document.querySelector('.mode').textContent = "Mode: Not Coloring"
+    }
+    }
 });
