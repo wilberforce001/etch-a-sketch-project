8fc41ee060607449d12ab2fdaa9adad7f8351b53
wilberforce001 <kenmisik@gmail.com>
2023-05-15 12:26:03 +0300

    Worked on the board to create the number and size of columns and rows on the board

---
+++ etch-a-sketch/index.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function(){
+    console.log("Hello")
+    let board = document.querySelector(".board");
+
+    board.style.gridTemplateColumns = "repeat(16, 1fr)";
+    board.style.gridTemplateRows = "repeat(16, 1fr)";
+})
+
+// 
