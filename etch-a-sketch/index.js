b43a4da8a290a175bcac30a04630aeb019df8a00
wilberforce001 <kenmisik@gmail.com>
2023-05-15 12:33:33 +0300

    Created a function that would change the number and size of the squares on the board

--- etch-a-sketch/index.js
+++ etch-a-sketch/index.js
@@ -1,9 +1,14 @@
 document.addEventListener("DOMContentLoaded", function(){
     console.log("Hello")
+    createBoard(32);
+    
+})
+
+function createBoard(size) {
     let board = document.querySelector(".board");
 
-    board.style.gridTemplateColumns = "repeat(16, 1fr)";
-    board.style.gridTemplateRows = "repeat(16, 1fr)";
-})
+    board.style.gridTemplateColumns = `repeat(${size}, 1fr)`
+    board.style.gridTemplateRows = `repeat(${size}, 1fr)`
+}
 
 // 
